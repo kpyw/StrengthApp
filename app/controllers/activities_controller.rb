56d252c85bd957a69ac3adf17c607dd5bdce3308
kpy <kate@drkatewolin.com>
2013-10-21 22:14:38 -0500

    clean up html
    move nav bar to single html file, upload images

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -19,14 +19,63 @@ def exercise
       {exercise: 'Seated Row',
         description: 'Extend your legs and wrap middle of band around feet. Be sure band is secure by wrapping the middle around your feet so it won\'t slip. Grasp both ends of band with elbows straight. Pull band upward and back, bending elbows. Slowly return to starting position.',
         tip: 'Keep your knees and back straight.',
-        image: '/Users/KYW/SL projects/s4h/DSC_6384.jpg'
+        image: '/DSC_6384.jpg'
         },
       {exercise: 'Hip Abductor',
-        description: 'Begin by looping the middle of the band around the ankle of the exercising leg. Place the ends of the band under the opposite foot to stabilize the band and grasp the ends in your opposite hand. Keeping your knees straight, kick your leg outward against the band. Hold and slowly return',
+        description: 'Begin by looping the middle of the band around the ankle of the exercising leg. Place the ends of the band under the opposite foot to stabilize the band and grasp the ends in your opposite hand. Keeping your knees straight, kick your leg outward against the band. Hold and slowly return.',
         tip: 'Keep your back straight; don\'t lean over.',
-        image: '/Users/KYW/SL projects/s4h/DSC_6989,jpg'
-        }
-
+        image: '/DSC_6989,jpg'
+        },
+      {exercise: 'Shoulder Pull Back',
+        description: 'Grasp the ends of the band in front of you at shoulder height, and take up the slack. Pull the band outward, keeping your elbows straight and pinch your shoulder blades together. Slowly return.',
+        tip: 'Keep your lower back straight. Don\'t hold your breath, and relax after each repetition.',
+        image: '/DSC_6560.jpg'
+        },
+      {exercise: 'Knee Chair Squat',
+        description: 'Begin with center of band under feet. Grasp ends of bands with hands by sides. Keep tension in the band with elbows straight. Keeping your elbows straight, slowly bend your knees while leaning forward slightly at the hips. Slowly return to starting position.',
+        tip: 'Keep your back straight.',
+        image: ' '
+        },
+      {exercise: 'Calf Raise',
+        description: 'Place the middle of the band under the balls of both feet. Grasp the ends of the band at your hips. Keeping your elbows straight, raise your heels off the floor. Hold and slowly return.',
+        tip: '',
+        image: ' '
+        },
+      {exercise: 'Leg Press Standing',
+        description: 'Begin by wrapping the middle of the band around your foot. Grasp the ends of the band at your chest, taking up the slack while you life your knee upward. Press your foot downward into the band, extending your hip and knee. Hold and slowly return.',
+        tip: 'Keep your back straight. Don’t lean over.',
+        image: ' '
+        },
+      {exercise: 'Hip Extension',
+        description: 'Begin by looping the middle of the band around the ankle of the exercising leg. Place the ends of the band under the opposite foot to stabilize the band and grasp the ends in your opposite hand. Keeping your knee straight, extend your leg backwards against the band. Hold and slowly return.',
+        tip: 'Keep your back straight; don’t lean over.',
+        image: ' '
+        },
+      {exercise: 'Sitting Hip Flex',
+        description: 'Sit in a sturdy chair. Begin by looping the center of the band around the top of the knee of your exercising leg. Bring the ends of the band underneath the foot of the opposite leg to stabilize and grasp the ends by your knee. Slowly flex your hip against the band, pulling upward. Hold and slowly return.',
+        tip: 'Keep your back straight; don’t lean forward.',
+        image: ' '
+        },
+      {exercise: 'Lat Pulldown',
+        description: 'Grasp the ends of the band above shoulder height with your elbows extended. Bend your elbows and bring your hands to your shoulders, pulling the band down and back. Hold and slowly return.',
+        tip: 'Keep your back and neck straight.',
+        image: ' '
+        },
+      {exercise: 'Overhead Press',
+        description: 'Place center of the band securely under one foot, and grasp each end of the band with palms facing forward. Lift arms forward & upward, extending your elbows with palms facing forward. Slowly return to the starting position.',
+        tip: 'Keep your back and neck straight. Don’t lift above your shoulders if it causes pain. Don\'t hold your breath. Relax between repetitions.',
+        image: ' '
+        },
+      {exercise: 'Forward Reach',
+        description: 'Begin with band wrapped around your upper back. Grasp both ends of band with elbows bent and palms facing inward. Push band forward, extending your elbows to shoulder level. Slowly return to starting position.',
+        tip: ' ',
+        image: ' '
+        },
+      {exercise: 'Upright Row',
+        description: 'Place middle of the band under both feet and grasp each end of the band with palms facing down. Pull the ends of the band upward toward your chin, lifting your elbows upward. Hold and slowly return to the starting position.',
+        tip: 'This exercise can be done seated or standing. You may find you need a bit more support in your “core” (the muscles in your abdomen and stomach area) at the beginning. If so, try sitting until your core strength grows. ',
+        image: ' '
+        },
       ]
   end
 end
